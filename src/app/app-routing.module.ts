0cd00a698b4d54931e4b619d0716f41cd73e24a4
rtorricodev <rodrigo.torricodelcastillo@gmail.com>
2022-10-23 11:05:19 -0400

    implemented initial structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
-import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { NgModule } from '@angular/core';
+
+const routes: Routes = [
+  {
+    path: "",
+    loadChildren: () => import('./modules/apartment-map/apartment-map.module').then((m) => m.ApartmentMapModule),
+    pathMatch: "full", 
+  },
+  {
+    path: "**",
+    redirectTo: "",
+    pathMatch: "full",
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
