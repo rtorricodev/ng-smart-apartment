1a1631aa7e3b0a6f6e7cfc8ad52905347c775211
rtorricodev <rodrigo.torricodelcastillo@gmail.com>
2022-10-28 02:11:28 -0400

    fixes

--- src/app/apartment-map/constants/apartment-map-config.const.ts
+++ src/app/apartment-map/constants/apartment-map-config.const.ts
@@ -1,10 +1,10 @@
 import { MapConfig } from '@shared/interfaces/map-config.interface';
 
 export const APARTMENT_MAP_CONFIG: MapConfig = {
-    center: [-96.790852,32.780843],
+    center: [-96.790852, 32.94473],
     container: "apartment-map",
     style: "https://api.maptiler.com/maps/eef16200-c4cc-4285-9370-c71ca24bb42d/style.json?key=CH1cYDfxBV9ZBu1lHGqh",
-    zoom: 13,
+    zoom: 9,
     defaultMakerConfig: {
         color: "#E74C3B",
         draggable: false
